3d557249bcac492fe9a495393debd6a6a2c07539
ajz34 <ajz34@outlook.com>
2025-07-24 18:11:45 +0800

    add cargo feature control

--- rstsr-lapack-ffi/src/lib.rs
+++ rstsr-lapack-ffi/src/lib.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "blas")]
 pub mod blas;
+#[cfg(feature = "cblas")]
 pub mod cblas;
+#[cfg(feature = "lapack")]
 pub mod lapack;
+#[cfg(feature = "lapacke")]
 pub mod lapacke;
+#[cfg(feature = "lapacke_utils")]
 pub mod lapacke_utils;
