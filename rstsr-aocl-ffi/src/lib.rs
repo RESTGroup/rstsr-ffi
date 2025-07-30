ac152fb67154885bc7fe3caa9ba062d8eaa1af52
ajz34 <ajz34@outlook.com>
2025-07-30 13:12:53 +0800

    aocl -- blis and flame

--- rstsr-aocl-ffi/src/lib.rs
+++ rstsr-aocl-ffi/src/lib.rs
@@ -1,1 +1,19 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
 
+pub mod blis_types;
+pub use blis_types::*;
+
+#[cfg(feature = "blis")]
+pub mod blis;
+
+#[cfg(feature = "blis")]
+pub use blis as blas;
+#[cfg(feature = "blis")]
+pub use blis as cblas;
+
+#[cfg(any(feature = "lapack", feature = "flame"))]
+pub mod flame;
+#[cfg(any(feature = "lapack", feature = "flame"))]
+pub use flame as lapack;
